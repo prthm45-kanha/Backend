94114b3d503bd24ff0407402f46039f51ade6f25
Pratham Agrawal <agrawalpratham2706@gmail.com>
2026-06-09 23:30:05 +0530

    setting up ejs file and learn how to do calculation

--- index.js
+++ index.js
@@ -3,9 +3,10 @@ const app=express();
 
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
+app.set('view engine','ejs');
 
 app.get('/',(req,res)=>{
-    res.send('chal rha h');
+    res.render('index');
 })
 app.listen(3000,()=>{
     console.log('Server is running on port 3000');
