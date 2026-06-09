f18638703cdaf5b56d56e6dbf865a342b54dc54d
Pratham Agrawal <agrawalpratham2706@gmail.com>
2026-06-09 23:53:19 +0530

    done with setting static public file i.e.images,stylsheets etc

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const app=express();
+const path=require('path');
 
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
+app.use(express.static(path.join(__dirname,'publicc')));
 app.set('view engine','ejs');
 
 app.get('/',(req,res)=>{
