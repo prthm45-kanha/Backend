a304c96f89a0ee34e0fa56f509b5f1e2f09c1205
Pratham Agrawal <agrawalpratham2706@gmail.com>
2026-06-13 19:29:37 +0530

    done with CSS

--- index.js
+++ index.js
@@ -1,4 +1,27 @@
-const express = require('express');
+// const express = require('express');
+// const app=express();
+// const path=require('path');
+
+// app.use(express.json());
+// app.use(express.urlencoded({extended:true}));
+
+// app.use(express.static(path.join(__dirname,'publicc')));
+
+// app.set('view engine','ejs');
+// app.get('/',(req,res)=>{
+//     res.render('index');
+// }) 
+// app.get('/profile/:username',(req,res)=>{
+//     res.send(`welcome back,${req.params.username}`)
+// })
+// app.get('/profile/:kuchbhilikhskteh/:age',(req,res)=>{
+//     res.send(`welcome back ${req.params.kuchbhilikhskteh} of age ${req.params.age}`)
+// })
+// app.listen(3000,()=>{
+//     console.log('Server is running on port 3000');
+// })
+
+const express= require('express');
 const app=express();
 const path=require('path');
 
@@ -6,17 +29,18 @@ app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 
 app.use(express.static(path.join(__dirname,'publicc')));
-
 app.set('view engine','ejs');
+
+
 app.get('/',(req,res)=>{
     res.render('index');
-}) 
+});
 app.get('/profile/:username',(req,res)=>{
-    res.send(`welcome back,${req.params.username}`)
-})
+    res.send(`welcome back ${req.params.username}`);
+});
 app.get('/profile/:kuchbhilikhskteh/:age',(req,res)=>{
-    res.send(`welcome back ${req.params.kuchbhilikhskteh} of age ${req.params.age}`)
-})
+    res.send(`welcome back ${req.params.kuchbhilikhskteh} of age ${req.params.age}`);
+});
 app.listen(3000,()=>{
     console.log('Server is running on port 3000');
 })
