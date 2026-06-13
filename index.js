97190e39b59cdcc131052c425754da4a0df01401
Pratham Agrawal <agrawalpratham2706@gmail.com>
2026-06-13 20:11:46 +0530

    done with creating task

--- index.js
+++ index.js
@@ -1,46 +1,36 @@
-// const express = require('express');
-// const app=express();
-// const path=require('path');
-
-// app.use(express.json());
-// app.use(express.urlencoded({extended:true}));
-
-// app.use(express.static(path.join(__dirname,'publicc')));
-
-// app.set('view engine','ejs');
-// app.get('/',(req,res)=>{
-//     res.render('index');
-// }) 
-// app.get('/profile/:username',(req,res)=>{
-//     res.send(`welcome back,${req.params.username}`)
-// })
-// app.get('/profile/:kuchbhilikhskteh/:age',(req,res)=>{
-//     res.send(`welcome back ${req.params.kuchbhilikhskteh} of age ${req.params.age}`)
-// })
-// app.listen(3000,()=>{
-//     console.log('Server is running on port 3000');
-// })
-
 const express= require('express');
 const app=express();
 const path=require('path');
+const fs=require('fs');
 
+app.set('view engine','ejs');
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
-
 app.use(express.static(path.join(__dirname,'publicc')));
-app.set('view engine','ejs');
 
 
 app.get('/',(req,res)=>{
-    res.render('index');
+    fs.readdir(`./files`,(err,files)=>{
+    res.render('index',{files:files});
+        
+    })
 });
-app.get('/profile/:username',(req,res)=>{
-    res.send(`welcome back ${req.params.username}`);
-});
-app.get('/profile/:kuchbhilikhskteh/:age',(req,res)=>{
-    res.send(`welcome back ${req.params.kuchbhilikhskteh} of age ${req.params.age}`);
-});
+
+app.post('/create',(req,res)=>{
+    fs.writeFile(`./files/${req.body.titlee.split(' ').join('')}.txt`,req.body.details,(err)=>{
+        res.redirect('/')
+    });
+    
+})
+
+// dynamic routing
+// app.get('/profile/:username',(req,res)=>{
+//     res.send(`welcome back ${req.params.username}`);
+// });
+// app.get('/profile/:kuchbhilikhskteh/:age',(req,res)=>{
+//     res.send(`welcome back ${req.params.kuchbhilikhskteh} of age ${req.params.age}`);
+// });
+
 app.listen(3000,()=>{
     console.log('Server is running on port 3000');
 })
